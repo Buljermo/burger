9bd13384db7368994db3499d15e40f8bbcfa590a
Dmitrii Popov <dpopov@neutroni.hayo.fi>
2023-03-08 21:34:03 +0200

    tietokantakäsittelyn kääreluokka

--- src/view/template.php
+++ src/view/template.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html lang="fi">
   <head>
-    <title>burger - <?=$this->e($title)?></title>
+    <title>Turo's Burger - <?=$this->e($title)?></title>
     <meta charset="UTF-8">    
   </head>
   <body>
     <header>
-      <h1>burger</h1>
+      <h1>Turo's Burger</h1>
     </header>
     <section>
       <?=$this->section('content')?>
     </section>
     <footer>
       <hr>
-      <div>burger by Kurpitsa</div>
+      <div>Turo's Burger by Kurpitsa</div>
     </footer>
   </body>
 </html>
