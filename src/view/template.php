e189deb02e5760710940fe44e89c8b2b9a9ef790
Dmitrii Popov <dpopov@neutroni.hayo.fi>
2023-03-09 18:16:44 +0200

    navigointi lisätty

--- src/view/template.php
+++ src/view/template.php
@@ -1,20 +1,27 @@
 <!DOCTYPE html>
 <html lang="fi">
-  <head>
-    <title>Turo's Burger - <?=$this->e($title)?></title>
-    <meta charset="UTF-8">
-    <link href="styles/styles.css" rel="stylesheet">    
-  </head>
-  <body>
+
+<head>
+  <title>Turo's Burger - <?= $this->e($title) ?></title>
+  <meta charset="UTF-8">
+  <link href="styles/styles.css" rel="stylesheet">
+</head>
+
+<body>
   <header>
-      <h1><a href="<?=BASEURL?>">Turo's Burger</a></h1>
-    </header>
-    <section>
-      <?=$this->section('content')?>
-    </section>
-    <footer>
-      <hr>
-      <div>Turo's Burger by Kurpitsa</div>
-    </footer>
-  </body>
+    <h1><a href="<?= BASEURL ?>">Turo's Burger</a></h1>
+  </header>
+  <nav>
+    <!-- <div><a href="<?=BASEURL?>"><img src="<?=BASEURL."/images/home.png"?>" alt="Etusivu"></a></div> -->
+    <div><a href="<?=BASEURL?>">Etusivu</a></div>
+  </nav>
+  <section>
+    <?= $this->section('content') ?>
+  </section>
+  <footer>
+    <hr>
+    <div>Turo's Burger by Kurpitsa</div>
+  </footer>
+</body>
+
 </html>
